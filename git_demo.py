17034049b5c693b3c93786d1e6b1267634b29bcb
naonao <nnrocks.off@gmail.com>
2021-07-22 10:53:34 +0800

    7th

--- git_demo.py
+++ git_demo.py
@@ -22,21 +22,7 @@
 # 任意のcommitに戻す
 # git log
 # commit XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-# // ３，git logでどこの部分まで戻したいかを確認できたらcommitの隣に書いてあるID的なものを入力
 # git reset --hard <commitID>
-
-# // ４（ここでエラー発生したら手順６へ），現在連携しているリポジトリを削除する
-# git remote rm <現在接続しているリポジトリのURL>
-
-# // ５，新しく連携するリモートリポジトリを登録する
-# git remote add <本来のリポジトリのURL>
-
-# // ６（５ができたらここはやる必要無し），しかし今回の場合はGithub上から元々連携していたリポジトリが削除されていたので（絶対やっちゃダメ）URLを変更することで対処
-# git remote set-url origin <本来のリポジトリのURL>
-
-# // ７，最後は強引にmain or masterブランチにpushして内容を反映させる
-# git push -f origin main or master
 
 print('This is Git Demo....')
 print('This is Git Demo....')
