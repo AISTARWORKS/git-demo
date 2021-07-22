6109bd5ba32ede05308357713b27ac5479fd6cca
naonao <nnrocks.off@gmail.com>
2021-07-22 09:53:57 +0800

    3rd

--- git_demo.py
+++ git_demo.py
@@ -1,4 +1,11 @@
 # This is Git Demo
 
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/AISTARWORKS/git-demo2.git
+# git push -u origin main
+
 print('This is Git Demo....')
 print('This is Git Demo....')
